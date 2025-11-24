aaab6e734273d3727d9b41505619e016d8227776
WWAXDX <abdullaharrxx@gmail.com>
2025-11-24 14:01:43 +0300

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+  path: 'home',
+  loadComponent: () =>
+    import('./modules/home/home').then(c => c.Home)
+},
+{
+  path: '',
+  redirectTo: 'home',
+  pathMatch: 'full'
+}
+];
